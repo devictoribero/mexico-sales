11f629469d1bf79d741ffa26a15ab48172398790
Victor Ribero <victor.ribero3@gmail.com>
2022-02-08 01:15:26 +0100

    added contact on each product

--- components/ProductInfo/index.js
+++ components/ProductInfo/index.js
@@ -1,7 +1,11 @@
-import { Flex, Text } from "@chakra-ui/react";
+import { Box, Flex, Text } from "@chakra-ui/react";
+import { whatsappHref } from "../../lib/contact";
+import { Hyperlink } from "../Hyperlink";
 import { ProductPrice } from "../ProductPrice";
 import { ProductTitle } from "../ProductTitle";
 
+const productContactText =
+  'Hola Gabriela! He visto los disitntos productos de la web y me ha gustado el "{productName}". Aun lo tienes?';
 export function ProductInfo({
   name,
   price,
@@ -9,13 +13,23 @@ export function ProductInfo({
   informative_text,
   ...props
 }) {
+  const dynamicContactText = productContactText.replace("{productName}", name);
   return (
-    <Flex alignItems="flex-start" justifyContent={"space-between"} {...props}>
-      <Flex flexDirection="column">
-        <ProductTitle mr="20px">{name}</ProductTitle>
-        {informative_text && <Text fontSize="xs">{informative_text}</Text>}
+    <Box>
+      <Flex alignItems="flex-start" justifyContent={"space-between"} {...props}>
+        <Flex flexDirection="column">
+          <ProductTitle mr="20px">{name}</ProductTitle>
+          {informative_text && <Text fontSize="xs">{informative_text}</Text>}
+        </Flex>
+        <ProductPrice>{price}</ProductPrice>
       </Flex>
-      <ProductPrice>{price}</ProductPrice>
-    </Flex>
+      <Hyperlink
+        fontSize="xs"
+        display="block"
+        href={whatsappHref(dynamicContactText)}
+      >
+        Pedir por whatsapp
+      </Hyperlink>
+    </Box>
   );
 }
